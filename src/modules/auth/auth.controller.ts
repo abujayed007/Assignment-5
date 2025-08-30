05258615ecb9c8bd0680d4a1a573da9982d42b15
abujayed007 <abujayed007@gmail.com>
2025-08-30 19:24:41 +0600

    logout done

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { AuthServices } from "./auth.service";
 import { sendResponse } from "../../utils/sendResponse";
@@ -18,6 +18,30 @@ const credentialLogin = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Logout successfully",
+      data: null,
+    });
+  }
+);
+
 export const AuthControllers = {
   credentialLogin,
+  logout,
 };
--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -4,5 +4,6 @@ import { AuthControllers } from "./auth.controller";
 const router = Router();
 
 router.post("/login", AuthControllers.credentialLogin);
+router.post("/logout", AuthControllers.logout);
 
 export const AuthRoute = router;
